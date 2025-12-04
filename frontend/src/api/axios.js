b76ead91bd9d919fec8fdfb8408815f3db28e132
dicmaryzambrano <dicmaryzambrano@outlook.com>
2025-12-04 02:53:54 -0400

    created notification system

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { pushAxiosNotification } from "../contexts/axiosNotify";
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
@@ -15,16 +16,18 @@ api.interceptors.request.use((config) => {
 // Response interceptor for refresh logic
 api.interceptors.response.use(
   (response) => response,
+
   async (error) => {
+    pushAxiosNotification(error);
+
     const originalRequest = error.config;
 
     // If unauthorized, try refresh
     if (error.response?.status === 401 && !originalRequest._retry) {
       originalRequest._retry = true;
 
-      const refreshToken = localStorage.getItem("refresh_token");
-
       try {
+        const refreshToken = localStorage.getItem("refresh_token");
         const res = await api.post("/auth/refresh", {
           refresh_token: refreshToken,
         });
