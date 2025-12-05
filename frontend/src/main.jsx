219b89419132c75b4b7ca4d7dfcaad26dee7655c
ElRodFe <el.rod.fe@gmail.com>
2025-12-05 20:55:48 -0300

    added toast provider to main file

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { ToastProvider } from "./utils/useToast";
 import './styles/fonts.css';
 import './styles/global.css';
 import "./styles/utilities.css";
@@ -7,6 +8,8 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <ToastProvider>
+      <App />
+    </ToastProvider>
   </StrictMode>,
 )
